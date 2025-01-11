2a206c475f061ffffe6fcd332a7fe6c76232b37a
Anujb2001 <146002516+Anujb2001@users.noreply.github.com>
2025-01-11 21:24:46 +0000

    Change the server address

--- flask_app/app.py
+++ flask_app/app.py
@@ -53,7 +53,7 @@ def preprocess_comment(comment):
 # Load the model and vectorizer from the model registry and local storage
 def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
     # Set MLflow tracking URI to your server
-    mlflow.set_tracking_uri("http://ec2-16-170-234-60.eu-north-1.compute.amazonaws.com:5000/")  # Replace with your MLflow tracking URI
+    mlflow.set_tracking_uri("http://ec2-13-51-176-80.eu-north-1.compute.amazonaws.com:5000/")  # Replace with your MLflow tracking URI
     client = MlflowClient()
     model_uri = f"models:/{model_name}/{model_version}"
     print("model_uri is: ", model_uri)
--- src/config/dev.py
+++ src/config/dev.py
@@ -1,1 +1,1 @@
-tracking_uri = "http://ec2-16-170-234-60.eu-north-1.compute.amazonaws.com:5000/"
+tracking_uri = "http://ec2-13-51-176-80.eu-north-1.compute.amazonaws.com:5000/"
